ab995cdc0c5ccfac74d6d47f88f387a631017349
Khayyam Islamzade <khayyamislamzade@gmail.com>
2020-10-02 16:00:20 +0400

    initial commit

---
+++ ExcelToXIF_XML/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace ExcelToXIF_XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.trafficFrInput = new System.Windows.Forms.TextBox();
+            this.fileChooseBtn = new System.Windows.Forms.Button();
+            this.fileChooseLbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.quantityOfGoodsInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.directionInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trNumberInput = new System.Windows.Forms.TextBox();
+            this.currencyTypeLbl = new System.Windows.Forms.Label();
+            this.currencyTypeInput = new System.Windows.Forms.TextBox();
+            this.trafficToLbl = new System.Windows.Forms.Label();
+            this.trafficToInput = new System.Windows.Forms.TextBox();
+            this.trafficFrLbl = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "fileDialog";
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.convertBtn.FlatAppearance.BorderColor = System.Drawing.Color.MediumSeaGreen;
+            this.convertBtn.FlatAppearance.BorderSize = 2;
+            this.convertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.convertBtn.Location = new System.Drawing.Point(8, 438);
+            this.convertBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(457, 54);
+            this.convertBtn.TabIndex = 0;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = false;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // trafficFrInput
+            // 
+            this.trafficFrInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.trafficFrInput.Location = new System.Drawing.Point(185, 47);
+            this.trafficFrInput.Margin = new System.Windows.Forms.Padding(4);
+            this.trafficFrInput.Name = "trafficFrInput";
+            this.trafficFrInput.Size = new System.Drawing.Size(237, 22);
+            this.trafficFrInput.TabIndex = 1;
+            this.trafficFrInput.Text = "840";
+            // 
+            // fileChooseBtn
+            // 
+            this.fileChooseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.fileChooseBtn.Location = new System.Drawing.Point(16, 365);
+            this.fileChooseBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.fileChooseBtn.Name = "fileChooseBtn";
+            this.fileChooseBtn.Size = new System.Drawing.Size(229, 48);
+            this.fileChooseBtn.TabIndex = 2;
+            this.fileChooseBtn.Text = "Choose File";
+            this.fileChooseBtn.UseVisualStyleBackColor = true;
+            this.fileChooseBtn.Click += new System.EventHandler(this.fileChooseBtn_Click);
+            // 
+            // fileChooseLbl
+            // 
+            this.fileChooseLbl.AutoSize = true;
+            this.fileChooseLbl.Location = new System.Drawing.Point(253, 381);
+            this.fileChooseLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fileChooseLbl.Name = "fileChooseLbl";
+            this.fileChooseLbl.Size = new System.Drawing.Size(15, 20);
+            this.fileChooseLbl.TabIndex = 3;
+            this.fileChooseLbl.Text = "-";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.quantityOfGoodsInput);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.directionInput);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.trNumberInput);
+            this.groupBox1.Controls.Add(this.currencyTypeLbl);
+            this.groupBox1.Controls.Add(this.currencyTypeInput);
+            this.groupBox1.Controls.Add(this.trafficToLbl);
+            this.groupBox1.Controls.Add(this.trafficToInput);
+            this.groupBox1.Controls.Add(this.trafficFrLbl);
+            this.groupBox1.Controls.Add(this.trafficFrInput);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(16, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(445, 323);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Values";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(9, 221);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "QUANTITY_OF_GOODS";
+            // 
+            // quantityOfGoodsInput
+            // 
+            this.quantityOfGoodsInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.quantityOfGoodsInput.Location = new System.Drawing.Point(185, 216);
+            this.quantityOfGoodsInput.Margin = new System.Windows.Forms.Padding(4);
+            this.quantityOfGoodsInput.Name = "quantityOfGoodsInput";
+            this.quantityOfGoodsInput.Size = new System.Drawing.Size(237, 22);
+            this.quantityOfGoodsInput.TabIndex = 15;
+            this.quantityOfGoodsInput.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(88, 177);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "DIRECTION";
+            // 
+            // directionInput
+            // 
+            this.directionInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.directionInput.Location = new System.Drawing.Point(185, 172);
+            this.directionInput.Margin = new System.Windows.Forms.Padding(4);
+            this.directionInput.Name = "directionInput";
+            this.directionInput.Size = new System.Drawing.Size(237, 22);
+            this.directionInput.TabIndex = 13;
+            this.directionInput.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(76, 266);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "TR_NUMBER";
+            // 
+            // trNumberInput
+            // 
+            this.trNumberInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.trNumberInput.Location = new System.Drawing.Point(185, 262);
+            this.trNumberInput.Margin = new System.Windows.Forms.Padding(4);
+            this.trNumberInput.Name = "trNumberInput";
+            this.trNumberInput.Size = new System.Drawing.Size(237, 22);
+            this.trNumberInput.TabIndex = 11;
+            this.trNumberInput.Text = "1";
+            // 
+            // currencyTypeLbl
+            // 
+            this.currencyTypeLbl.AutoSize = true;
+            this.currencyTypeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.currencyTypeLbl.Location = new System.Drawing.Point(47, 139);
+            this.currencyTypeLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.currencyTypeLbl.Name = "currencyTypeLbl";
+            this.currencyTypeLbl.Size = new System.Drawing.Size(124, 17);
+            this.currencyTypeLbl.TabIndex = 8;
+            this.currencyTypeLbl.Text = "CURRENCY TYPE";
+            // 
+            // currencyTypeInput
+            // 
+            this.currencyTypeInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.currencyTypeInput.Location = new System.Drawing.Point(185, 134);
+            this.currencyTypeInput.Margin = new System.Windows.Forms.Padding(4);
+            this.currencyTypeInput.Name = "currencyTypeInput";
+            this.currencyTypeInput.Size = new System.Drawing.Size(237, 22);
+            this.currencyTypeInput.TabIndex = 7;
+            this.currencyTypeInput.Text = "840";
+            // 
+            // trafficToLbl
+            // 
+            this.trafficToLbl.AutoSize = true;
+            this.trafficToLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.trafficToLbl.Location = new System.Drawing.Point(23, 96);
+            this.trafficToLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.trafficToLbl.Name = "trafficToLbl";
+            this.trafficToLbl.Size = new System.Drawing.Size(152, 17);
+            this.trafficToLbl.TabIndex = 6;
+            this.trafficToLbl.Text = "GOODS_TRAFFIC_TO";
+            // 
+            // trafficToInput
+            // 
+            this.trafficToInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.trafficToInput.Location = new System.Drawing.Point(185, 92);
+            this.trafficToInput.Margin = new System.Windows.Forms.Padding(4);
+            this.trafficToInput.Name = "trafficToInput";
+            this.trafficToInput.Size = new System.Drawing.Size(237, 22);
+            this.trafficToInput.TabIndex = 5;
+            this.trafficToInput.Text = "031";
+            // 
+            // trafficFrLbl
+            // 
+            this.trafficFrLbl.AutoSize = true;
+            this.trafficFrLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.trafficFrLbl.Location = new System.Drawing.Point(23, 51);
+            this.trafficFrLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.trafficFrLbl.Name = "trafficFrLbl";
+            this.trafficFrLbl.Size = new System.Drawing.Size(150, 17);
+            this.trafficFrLbl.TabIndex = 4;
+            this.trafficFrLbl.Text = "GOODS_TRAFFIC_FR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 509);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.fileChooseLbl);
+            this.Controls.Add(this.fileChooseBtn);
+            this.Controls.Add(this.convertBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel Convertor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //this.fileDialog = new OpenFileDialog();
+        //    this.convertBtn = new Button();
+        //    this.trafficFrInput = new TextBox();
+        //    this.fileChooseBtn = new Button();
+        //    this.fileChooseLbl = new Label();
+        //    this.groupBox1 = new GroupBox();
+        //    this.label3 = new Label();
+        //    this.quantityOfGoodsInput = new TextBox();
+        //    this.label2 = new Label();
+        //    this.directionInput = new TextBox();
+        //    this.label1 = new Label();
+        //    this.trNumberInput = new TextBox();
+        //    this.currencyTypeLbl = new Label();
+        //    this.currencyTypeInput = new TextBox();
+        //    this.trafficToLbl = new Label();
+        //    this.trafficToInput = new TextBox();
+        //    this.trafficFrLbl = new Label();
+        //    this.groupBox1.SuspendLayout();
+        //this.SuspendLayout();
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.TextBox trafficFrInput;
+        private System.Windows.Forms.Button fileChooseBtn;
+        private System.Windows.Forms.Label fileChooseLbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox quantityOfGoodsInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox directionInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox trNumberInput;
+        private System.Windows.Forms.Label currencyTypeLbl;
+        private System.Windows.Forms.TextBox currencyTypeInput;
+        private System.Windows.Forms.Label trafficToLbl;
+        private System.Windows.Forms.TextBox trafficToInput;
+        private System.Windows.Forms.Label trafficFrLbl;
+    }
+}
---
+++ ExcelToXIF_XML/Form1.cs
@@ -0,0 +1,258 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ExcelToXIF_XML
+{
+    public partial class Form1 : Form
+    {
+        private readonly Dictionary<string, string> _defaultValueDictionary;
+        private readonly KeyValuePair<string, string> _defaultTrafficFr = new KeyValuePair<string, string>("GOODS_TRAFFIC_FR", "840");
+        private readonly KeyValuePair<string, string> _defaultTrafficTo = new KeyValuePair<string, string>("GOODS_TRAFFIC_TO", "031");
+        private readonly KeyValuePair<string, string> _defaultCurrencyType = new KeyValuePair<string, string>("QUANTITY_OF_GOODS", "1");
+        private readonly KeyValuePair<string, string> _defaultDirection = new KeyValuePair<string, string>("DIRECTION", "1");
+        private readonly KeyValuePair<string, string> _defaultQuantityOfGoods = new KeyValuePair<string, string>("CURRENCY_TYPE", "840");
+        private int DefaultTrNumber = 1;
+        private bool _isFileSelected = false;
+
+        public Form1()
+        {
+            this._defaultValueDictionary = new Dictionary<string, string>();
+            this._defaultValueDictionary.Add(_defaultTrafficFr.Key, _defaultTrafficFr.Key);
+            this._defaultValueDictionary.Add(_defaultTrafficTo.Key, _defaultTrafficTo.Key);
+            this._defaultValueDictionary.Add(_defaultCurrencyType.Key, _defaultCurrencyType.Key);
+            this._defaultValueDictionary.Add(_defaultDirection.Key, _defaultDirection.Key);
+            this._defaultValueDictionary.Add(_defaultQuantityOfGoods.Key, _defaultQuantityOfGoods.Key);
+
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void fileChooseBtn_Click(object sender, EventArgs e)
+        {
+            using (this.fileDialog)
+            {
+                this.fileDialog.Filter = @"Excel Files|*.xls;*.xlsx;*.xlsm";
+                if (this.fileDialog.ShowDialog() != DialogResult.OK)
+                    return;
+                string fileName = this.fileDialog.FileName;
+                this.fileChooseLbl.Text = Path.GetFileNameWithoutExtension(fileName) + Path.GetExtension(fileName);
+                this._isFileSelected = true;
+            }
+        }
+
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            if (this._isFileSelected)
+            {
+                try
+                {
+                    string fileName = this.fileDialog.FileName;
+
+                    string withoutExtension = Path.GetFileNameWithoutExtension(fileName);
+                    string directoryName = Path.GetDirectoryName(fileName);
+
+                    var excelAsList = this.ExcelToList(fileName);
+
+                    DateTime now = DateTime.Now;
+                    string uniqueString = string.Format("{0}_{1}_{2}_{3}_{4}", (object)now.Day, (object)now.Month,
+                        (object)now.Year, (object)now.Hour, (object)now.Minute);
+                    XDocument xml = this.ListToXml(excelAsList);
+                    string uniqueFileName = withoutExtension + "_" + uniqueString + ".xml";
+                    var path = Path.Combine(directoryName ?? string.Empty, uniqueFileName);
+                    xml.Save(path);
+                    int num = (int)MessageBox.Show("File Saved As " + uniqueFileName, "Success");
+                }
+                catch (Exception ex)
+                {
+                    if (ex is ArgumentException || ex is PathTooLongException)
+                    {
+                        MessageBox.Show("Choose File");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error", ex.InnerException?.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Choose File");
+            }
+        }
+
+
+        private List<List<string>> ExcelToList(string path)
+        {
+            var package = new ExcelPackage(new FileInfo(path));
+            var workBook = package.Workbook;
+            var worksheet = workBook.Worksheets[0];
+            List<List<string>> stringListList = new List<List<string>>();
+
+            var rowIndex = FindTableRowIndex(worksheet);
+            for (int row = rowIndex; row <= worksheet.Dimension.End.Row; ++row)
+            {
+                var isEmptyRow = IsEmptyRow(worksheet, row, 5);
+                if (!isEmptyRow)
+                {
+                    List<string> stringList = new List<string>();
+                    for (int col = 1; col < worksheet.Dimension.End.Column; ++col)
+                    {
+                        string str = "";
+                        if (worksheet.Cells[row, col].Value != null)
+                            str = worksheet.Cells[row, col].Value.ToString();
+                        stringList.Add(str);
+                    }
+
+                    stringListList.Add(stringList);
+                }
+
+            }
+
+            return stringListList;
+        }
+
+        private int FindTableRowIndex(ExcelWorksheet worksheet)
+        {
+            int nullColumnTolerance = 3;
+            for (int row = 1; row <= worksheet.Dimension.End.Row; row++)
+            {
+                var isEmptyRow = IsEmptyRow(worksheet, row, nullColumnTolerance);
+                if (!isEmptyRow)
+                {
+                    return row;
+                }
+            }
+
+            return 1;
+        }
+
+        private bool IsEmptyRow(ExcelWorksheet worksheet, int row = 1, int nullColumnTolerance = 3)
+        {
+            int nullValueCounter = 0;
+
+            //from 0 to 6
+            int nullColumnArrange = nullColumnTolerance + 3;
+
+            for (int col = 1; col < worksheet.Dimension.End.Column; ++col)
+            {
+                var cell = worksheet.Cells[row, col];
+                if (cell.Value == null)
+                    nullValueCounter++;
+
+                if (col >= nullColumnArrange)
+                {
+                    if (nullValueCounter >= nullColumnTolerance)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+
+            }
+            return false;
+        }
+
+
+        private XDocument ListToXml(List<List<string>> values)
+        {
+            XDeclaration declaration = new XDeclaration("1.0", "utf-8", "yes");
+            XElement mainElement = new XElement((XName)"GoodsInfo");
+
+            string trafficFr, trafficTo, currencyType, quantityOfGoods, direction;
+            int trNumber;
+            trafficFr = !string.IsNullOrWhiteSpace(this.trafficFrInput.Text)
+               ? this.trafficFrInput.Text
+               : this._defaultTrafficFr.Value;
+
+            trafficTo = !string.IsNullOrWhiteSpace(this.trafficToInput.Text)
+               ? this.trafficToInput.Text
+               : this._defaultTrafficTo.Value;
+
+            currencyType = !string.IsNullOrWhiteSpace(this.currencyTypeInput.Text)
+               ? this.currencyTypeInput.Text
+               : this._defaultCurrencyType.Value;
+
+            quantityOfGoods = !string.IsNullOrWhiteSpace(this.quantityOfGoodsInput.Text)
+               ? this.quantityOfGoodsInput.Text
+               : this._defaultQuantityOfGoods.Value;
+
+            direction = !string.IsNullOrWhiteSpace(this.directionInput.Text)
+               ? this.directionInput.Text
+               : this._defaultDirection.Value;
+
+            trNumber = !string.IsNullOrWhiteSpace(this.trNumberInput.Text)
+               ? Convert.ToInt32(this.trNumberInput.Text)
+               : this.DefaultTrNumber;
+
+
+
+
+            List<string> headers = values[0];
+            foreach (string key in _defaultValueDictionary.Select(c => c.Key))
+            {
+                var isdefaultHeaderExistInExcel = headers.Contains(key, StringComparer.OrdinalIgnoreCase);
+                //if header is exist in list , then change value for all rows
+                if (isdefaultHeaderExistInExcel)
+                {
+                    var indexOfHeader = headers.IndexOf(key);
+                    foreach (var val in values)
+                    {
+                        val[indexOfHeader] = key;
+                    }
+
+                }
+                //if not exist in list , then add 
+                else
+                {
+                    headers.Add(key);
+                    foreach (var clmn in values)
+                    {
+                        clmn.Add(key);
+                    }
+
+                }
+            }
+
+
+
+            //index 0 is headers
+            for (int i = 1; i < values.Count; ++i)
+            {
+                XElement xelement2 = new XElement((XName)"GOODS");
+                xelement2.Add((object)new XElement((XName)"TR_NUMBER", (object)trNumber++.ToString()));
+
+                //value
+                for (int j = 0; j < values[i].Count; ++j)
+                {
+                    string header = headers[j];
+                    string value = values[i][j];
+                    if (!string.IsNullOrEmpty(header))
+                        xelement2.Add((object)new XElement((XName)header, (object)value));
+                }
+
+
+                mainElement.Add((object)xelement2);
+            }
+
+            return new XDocument(declaration, new object[1]
+            {
+                (object) mainElement
+            });
+        }
+    }
+}
